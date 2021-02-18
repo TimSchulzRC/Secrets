949de3e5bef7960a44b23127e4907c7f0212daef
Tim Schulz <tim.schulz.rc@gmail.com>
2021-02-18 18:30:43 +0100

    password encryption and autofocus to email field

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 const app = express();
 
 app.use(express.static("public"));
@@ -14,10 +15,13 @@ app.use(
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+const secret = "Thisthesecretsectret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
