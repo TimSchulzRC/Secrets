66ca49c8b845da1f0dc2d22122530389cb4c0ec9
Tim Schulz <tim.schulz.rc@gmail.com>
2021-02-18 21:04:39 +0100

    check if user exists

--- app.js
+++ app.js
@@ -68,15 +68,26 @@ app
   })
 
   .post((req, res) => {
-    const newUser = new User({
-      email: req.body.username,
-      password: md5(req.body.password),
-    });
-    newUser.save((err) => {
-      if (err) {
-        console.log(err);
+    username = req.body.username;
+    User.exists({ email: username }, function (err, exists) {
+      if (!err) {
+        if (!exists) {
+          const newUser = new User({
+            email: req.body.username,
+            password: md5(req.body.password),
+          });
+          newUser.save((err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.render("secrets");
+            }
+          });
+        } else {
+          res.redirect("#");
+        }
       } else {
-        res.render("secrets");
+        console.log(err);
       }
     });
   });
